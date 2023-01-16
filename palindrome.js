afd487b37012d1ee3543f1d7b577f14bdf51f72d
Tanmay <meetsunny0905@gmail.com>
2023-01-16 21:36:26 +0530

    prime number and palindrome problem is done

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+ const reversedStr = str.split('').reverse().join();
+  if (reversedStr === str) {
+      console.log('Yes');
+  }
+  else{
+      console.log('No');
+  }
--- prime.js
+++ prime.js
@@ -1,5 +1,5 @@
 let a=0;
-    for(let i=1; i<num;i++){
+    for(let i=1; i<=num;i++){
         if(num%i===0){
             a++;
         }
