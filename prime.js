0c2ae5a9eb429ac43822b10f0412624238bc2851
Tanmay <meetsunny0905@gmail.com>
2023-01-16 21:26:31 +0530

    prime number problem is done

---
+++ prime.js
@@ -0,0 +1,12 @@
+let a=0;
+    for(let i=1; i<num;i++){
+        if(num%i===0){
+            a++;
+        }
+    }
+    if(a>1){
+        console.log("No");
+    }
+    else{
+        console.log("Yes");
+    }
